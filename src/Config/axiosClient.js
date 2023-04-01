ba29fd6bd23568e99378b16f973df9c89ea8eaa2
ThomasAnderson23 <renavarr@uc.cl>
2023-04-01 09:20:14 -0300

    update axios

--- src/Config/axiosClient.js
+++ src/Config/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: "http://https://backend-reveco.onrender.com"
+    baseURL: "https://backend-reveco.onrender.com"
 })
 
 export default axiosClient;
